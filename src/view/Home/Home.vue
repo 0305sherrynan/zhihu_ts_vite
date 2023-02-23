9348907390e52f9ec73f2b8684c40bdf8989ea57
zheng <2642198391@qq.com>
2023-02-23 19:15:51 +0800

    feat（view视图层->Home）:新增加载功能 完善加载逻辑 bug：3的倍数的数据如何判断为最后一页

--- src/view/Home/Home.vue
+++ src/view/Home/Home.vue
@@ -10,27 +10,46 @@
         </section>
         <span>发现精彩</span>
         <column-list :list="store.columns.data"></column-list>
+        <div class="btn-loader" @click="loadMorePage" v-if="!isLastPage">
+            <div class="loader-span-box">
+                <span>加载更多</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import ColumnList from '@/components/ColumnList.vue';
+import useLoadMore from '@/hooks/store/useLoadMore';
 import { useUserStore } from '@/store/user';
-import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { computed, onMounted,ref } from 'vue';
 
 /**
  * 数据
  * store 获取用户存储信息
  */
 const store = useUserStore()
+const currentPage = computed(() => store.columns.currentPage)
+// const {columns} = storeToRefs(store)
+// const a = ref(store.columns.currentPage)
+// const currentPage =ref(columns.value.currentPage)
+/**
+ * 方法
+ */
 
 /**
  * 操作
  * onMounted fetchColumns() 获取列表信息
+ * hook 获取 loadMorePage 方法、isLastPage 数据
  */
 onMounted(()=>{
-    store.fetchColumns()
+    store.fetchColumns().then(()=>{
+        // currentPage.value++
+    })
 })
+
+const {loadMorePage,isLastPage} = useLoadMore(currentPage.value+1,3)
 </script>
 
 <style scoped lang="less">
@@ -62,6 +81,18 @@ onMounted(()=>{
         font-size: 25px;
         font-weight: 400;
 
+    }
+    >.btn-loader{
+        margin: 30px 0;
+        >.loader-span-box{
+            border: 1px solid rgb(52, 133, 253);
+            display: inline-block;
+            border-radius: 4px;
+            /* padding: 10px; */
+            padding: 6px 120px 6px 120px;
+            color: rgb(13, 110, 253);
+            font-weight: 549;
+        }
     }
 }
 </style>
