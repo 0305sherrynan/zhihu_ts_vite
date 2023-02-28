f4f6aa00246f45cebb78614ac828608e65f435bb
zheng <2642198391@qq.com>
2023-02-28 15:57:12 +0800

    feat（oftenUI->validateForm）:简易的表单 包括default和submit两个插槽

---
+++ src/base/oftenUI/ValidateForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <form action="">
+            <slot name="default"></slot>
+            <div>
+                <slot name="submit"></slot>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { ValidateFunc, mitter } from '@/libs/mitt';
+// import { type } from 'os';
+
+
+const funcArr:ValidateFunc[] = []
+const callback = (func?:ValidateFunc)=>{
+    // if (func) funcArr.push(func)
+    console.log(123)
+}
+
+mitter.on('form-item-created',callback)
+</script>
+
+<style scoped>
+
+</style>
