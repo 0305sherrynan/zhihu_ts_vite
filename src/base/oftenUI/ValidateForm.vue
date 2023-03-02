d22649f18ae8741021c08442c54245bd0b511d75
zheng <2642198391@qq.com>
2023-03-02 14:35:53 +0800

    feat（oftenUI->validateForm）：增加按钮点击时触发emit

--- src/base/oftenUI/ValidateForm.vue
+++ src/base/oftenUI/ValidateForm.vue
@@ -2,7 +2,7 @@
     <div>
         <form action="">
             <slot name="default"></slot>
-            <div>
+            <div @click="submitForm">
                 <slot name="submit"></slot>
             </div>
         </form>
@@ -14,11 +14,22 @@
 import { ValidateFunc, mitter } from '@/libs/mitt';
 // import { type } from 'os';
 
+const emit = defineEmits(['form-submit'])
 
 const funcArr:ValidateFunc[] = []
+
+/**
+ * method
+ * callback @param func 由ValidateInput传来的验证函数
+ * submitForm 按钮点击 触发事件 进而将最终验证结果发送给上一级组件
+ */
 const callback = (func?:ValidateFunc)=>{
-    // if (func) funcArr.push(func)
+    if (func) funcArr.push(func)
     console.log(123)
+}
+const submitForm = ()=>{
+    const result = funcArr.map(func=>func()).every(result=>result)
+    emit('form-submit',result)
 }
 
 mitter.on('form-item-created',callback)
