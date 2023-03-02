ab79dc46e9b2ec27b1e65914a27fe2227fbdcaaf
zheng <2642198391@qq.com>
2023-03-02 14:44:37 +0800

    feat（dao层->sql语句）：新增展示Home列表sql语句

--- src/server/dao/map/listMap.js
+++ src/server/dao/map/listMap.js
@@ -1,14 +1,16 @@
 const listSqlMap = {
     /**
      * map层：mysql语句
-     * showUserInfo 展示所有的list信息
-     * loginSearch 验证登录是否成功
+     * showListInfo 展示所有的list信息
+     * postData 提交数据
      * deleteInfoById 删除指定id的用户信息
      * addInfo 增加user信息
      * updateInfo 更新user信息
     */
     showListInfo:'select id,title,avatar,description from columnlist where columnlist.id >=? and columnlist.id <=?',
-    loginSearch:"select * from user  where username = ? and password = ?",
+    // loginSearch:"select * from user  where username = ? and password = ?",
+    postData:"insert into columnlist values(?,?,?,?,?)",
+    // postData:"",
     deleteInfoById:`delete from user where id = ?`,
     addInfo:'insert into user values(?,?,?,?)',
     updateInfo:'update user set username = ?,password = ?,id = ?,time = ? where id = ?'
