f910fcd711629b1144d1c8a72d7587721d91710c
zheng <2642198391@qq.com>
2023-03-02 14:40:18 +0800

    feat:新增post层提交的数据类型

--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -1,9 +1,14 @@
 export interface ColumnProps{
     id:string
     title:string
-    avatar?:string
+    avatar?:string | undefined
     description: string
 }
+
+interface commitUserData extends ColumnProps{
+    account:string
+}
+
 export interface ImageProps{
     id?:string,
     url?:string
