07057780621024d7f8de8edd4de4411c1c63662a
zheng <2642198391@qq.com>
2023-03-02 14:41:57 +0800

    feat:ts增加注册接口使用和获取用户数据行数的数据类型

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -1,3 +1,5 @@
+import { type } from 'os'
+import {ColumnProps} from './store'
 export namespace userApi{
     interface ResponseSuccess<T>{
         code:boolean,
@@ -7,8 +9,17 @@ export namespace userApi{
         account:string,
         password:string
     }
+    //注册接口使用
+    interface registerAccount extends accountUser{
+        nickname:string
+    }
+    //用于 libs/api->getUserInfoRows 获取用户数据行数
+    type pickUserAccount = Pick<User, "account">
+    
     interface ResponseSuccessList<T>{
         code:boolean,
         data:Array<T>
     }
+
+
 }
