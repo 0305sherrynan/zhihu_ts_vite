441d46eab92e08587f70c1dbd284edd62aaf9a09
zheng <2642198391@qq.com>
2023-03-02 14:44:25 +0800

    feat（dao层->sql语句）：新增展示sql语句

--- src/server/dao/map/userMap.js
+++ src/server/dao/map/userMap.js
@@ -2,13 +2,15 @@ const userSqlMap = {
     /**
      * map层：mysql语句
      * showUserInfo 展示所有的user信息
+     * 
      * loginSearch 验证登录是否成功
      * deleteInfoById 删除指定id的用户信息
      * addInfo 增加user信息
      * updateInfo 更新user信息
     */
     showUserInfo:'select * from user where account = ? and password = ?',
-    loginSearch:"select * from user  where username = ? and password = ?",
+    registerAccount:"insert into user values(1,?,'fdsfsrh',?,?)",
+    infoRows:'select count(*) from columnlist where account = ?',
     deleteInfoById:`delete from user where id = ?`,
     addInfo:'insert into user values(?,?,?,?)',
     updateInfo:'update user set username = ?,password = ?,id = ?,time = ? where id = ?'
